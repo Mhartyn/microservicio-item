f543735d24222183fd591f84f205cbf7a7738137
mhartyn <mhartynpaez@hotmail.com>
2020-05-15 18:01:35 -0500

    api gateway

---
+++ microservicio-zuul-server/src/test/java/com/microservicio/zuul/microserviciozuulserver/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.zuul.microserviciozuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
