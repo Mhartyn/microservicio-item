1cfc5c4c2db38638f369ab076913dec1c71be72d
mhartyn <mhartynpaez@hotmail.com>
2020-05-16 14:16:00 -0500

    configuracion centralizada

---
+++ microservicio-config-server/src/test/java/com/microservicio/config/microservicioconfigserver/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.config.microservicioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- microservicio-item/src/main/java/com/microservicio/item/microservicioitem/controller/ItemController.java
+++ microservicio-item/src/main/java/com/microservicio/item/microservicioitem/controller/ItemController.java
@@ -1,14 +1,21 @@
 package com.microservicio.item.microservicioitem.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.microservicio.item.microservicioitem.models.Item;
 import com.microservicio.item.microservicioitem.models.Producto;
 import com.microservicio.item.microservicioitem.models.services.IItemServices;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemController {
     
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
     @Autowired
     @Qualifier("serviceFeing")
     //@Qualifier("serviceClientRest")
     private IItemServices itemService;
+
+    @Value("${configuration.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> Listar(){
@@ -34,5 +46,17 @@ public class ItemController {
 
     public Item VerPorDefecto(Long id, Integer cantidad){
         return new Item(new Producto(id, "Sin Descripcion", 0.0), cantidad);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+
+        log.info(texto);
+
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+        
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 }
