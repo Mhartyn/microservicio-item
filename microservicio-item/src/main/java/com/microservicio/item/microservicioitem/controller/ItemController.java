031b2bf410e41ed239ec0ac919f9b8a55fb93b68
mhartyn <mhartynpaez@hotmail.com>
2020-05-17 15:51:05 -0500

    configuraciones

--- microservicio-item/src/main/java/com/microservicio/item/microservicioitem/controller/ItemController.java
+++ microservicio-item/src/main/java/com/microservicio/item/microservicioitem/controller/ItemController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ItemController {
     
