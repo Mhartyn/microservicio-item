81a096eb2f7b5789d93260653e46d53bc282cf58
mhartyn <mhartynpaez@hotmail.com>
2020-05-20 19:11:29 -0500

    serguridad y usuarios

---
+++ microservicio-common-usuario/src/main/java/com/microservicio/common/usuario/microserviciocommonusuario/models/entity/Usuario.java
@@ -0,0 +1,168 @@
+package com.microservicio.common.usuario.microserviciocommonusuario.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String username;
+
+    public Usuario() {
+    }
+
+    public List<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
+    }
+
+    public Usuario(Long id, String username, String password, boolean enabled, String nombre, String apellido,
+            String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Usuario id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Usuario username(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public Usuario password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Usuario enabled(boolean enabled) {
+        this.enabled = enabled;
+        return this;
+    }
+
+    public Usuario nombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public Usuario apellido(String apellido) {
+        this.apellido = apellido;
+        return this;
+    }
+
+    public Usuario email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " id='" + getId() + "'" + ", username='" + getUsername() + "'" + ", password='" + getPassword()
+                + "'" + ", enabled='" + isEnabled() + "'" + ", nombre='" + getNombre() + "'" + ", apellido='"
+                + getApellido() + "'" + ", email='" + getEmail() + "'" + "}";
+    }
+
+    @Column(length = 60)
+    private String password;
+    private boolean enabled;
+    private String nombre;
+    private String apellido;
+
+    @Column(unique = true, length = 100)
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name= "usuario_id"), inverseJoinColumns = @JoinColumn(name= "rol_id"), uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "rol_id"})})
+    private List<Rol> roles;
+
+    private static final long serialVersionUID = 3951508127137032588L;
+    
+}
