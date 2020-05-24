d07f9b62aea2ca1b403988ccf7484d87d713a2a2
mhartyn <mhartynpaez@hotmail.com>
2020-05-23 19:00:35 -0500

    conexion a bds en mysql y postgreSQL

--- microservicio-usuario/src/main/resources/import.sql
+++ microservicio-usuario/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into usuarios (username, password, enabled, nombre, apellido, email) values('mhartyn', '$2a$10$0Yc.mMT97icF6TDhxtcV1.tjz48kOENkTQdI/2n2cQN89qhstJQiy', 1, 'martin', 'gutierrez', 'martin@correo.com')
-insert into usuarios (username, password, enabled, nombre, apellido, email) values('admin', '$2a$10$ww2XvzVhqZC5Y074ZCw7ceGXlcwithj4XuNEltgga0vgCkSQnYx/u', 1, 'administrador', 'sistema', 'admin@sistema.com')
+insert into usuarios (username, password, enabled, nombre, apellido, email) values('mhartyn', '$2a$10$0Yc.mMT97icF6TDhxtcV1.tjz48kOENkTQdI/2n2cQN89qhstJQiy', true, 'martin', 'gutierrez', 'martin@correo.com')
+insert into usuarios (username, password, enabled, nombre, apellido, email) values('admin', '$2a$10$ww2XvzVhqZC5Y074ZCw7ceGXlcwithj4XuNEltgga0vgCkSQnYx/u', true, 'administrador', 'sistema', 'admin@sistema.com')
 
 insert into roles(nombre) values('ROLE_USER')
 insert into roles(nombre) values('ROLE_ADMIN')
